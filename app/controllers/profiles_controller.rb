ea64488fb54e0a31491703337715bf6f3591854d
Alex Duvanenko <ajduvanenko@gmail.com>
2016-04-14 00:06:54 +0000

    Added form partial and edit action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,7 @@ class ProfilesController < ApplicationController
         # form where a user can fill out their own profile
         @user = User.find( params[:user_id] )
         @profile = Profile.new
-    
+    end
     def create
         @user = User.find( params[:user_id] )
         @profile = @user.build_profile(profile_params)
@@ -13,6 +13,11 @@ class ProfilesController < ApplicationController
         else
             render action: :new
         end
+    end
+    
+    def edit
+        @user = User.find( params[:user_id] )
+        @profile = @user.profile
     end
     
     private
